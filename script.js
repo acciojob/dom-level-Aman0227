cae8cf6e15db95005139b38ef940dca83c8a1f2f
Aman0227 <amansingh765487@gmail.com>
2025-10-05 09:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-//your JS code here. If required.
-cosnt target = document.getElementById("level")
-if(target){
-let level = 1;
-	let parent = target.parentElement;
-	while (parent){
-		level++;
-		parent = parent.parentElement;
-		}
-	alert("The level of the element is: " + level);
-} else {
-    alert("Element with id 'level' not found.");
+window.onload = function (){
+	let element = document.getElementById("level");
+	let level = 0;
+	while(el) {
+level++
+	el = el.parentElement
+	}
+	alert("The level of the element is" + level);
 }
